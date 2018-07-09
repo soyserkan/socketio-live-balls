41b33ab5f8ed385c9c65591200def326e795f4a2
soyserkan <serkansoy94@gmail.com>
2018-07-09 17:19:20 +0300

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,26 @@
-app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=>{
+app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=> {
 
-    const connectionOptions={
-        reconnectionAttempts:3,
+    $scope.init=()=>{
+
+        const username = prompt('Please enter username');
+
+    if (username)
+        initSocket(username);
+    else
+        return false;
+    };
+
+    function initSocket(username) {
+        const connectionOptions={
+            reconnectionAttempts:3,
             reconnectionDelay:600
-    }
+        }
 
-    indexFactory.connectSocket('http://localhost:3000',connectionOptions)
-        .then((socket)=>{
-        console.log('baglantı gerceklesti',socket);
-    }).catch((err)=>{
-        console.log(err);
-    });
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+            .then((socket)=>{
+                socket.emit('newUser',{username});
+            }).catch((err)=>{
+            console.log(err);
+        });
+    }
 }]);
