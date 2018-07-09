8ebbfbb400a27e66d906b1afc91ff7b51b83f0cf
soyserkan <serkansoy94@gmail.com>
2018-07-10 02:06:52 +0300

    animasyon islemi

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,6 +1,8 @@
 app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=> {
 
     $scope.messages=[];
+    $scope.players={};
+
     $scope.init=()=>{
 
         const username = prompt('Please enter username');
@@ -20,6 +22,12 @@ app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=
         indexFactory.connectSocket('http://localhost:3000',connectionOptions)
             .then((socket)=>{
                 socket.emit('newUser',{username});
+
+                socket.on('initPlayers',(players)=>{
+                    $scope.players=players;
+                    $scope.$apply();
+                })
+
                 socket.on('newUser',(data)=>{
                     const messageData={
                         type:{
@@ -46,6 +54,18 @@ app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=
                     $scope.$apply();
                 });
 
+
+                let animate=false;
+                $scope.onClickPlayer=($event)=>{
+                    if (!animate) {
+                        animate=true;
+                        $('#'+socket.id).animate({'left':$event.offsetX,'top':$event.offsetY});
+                        animate=false;
+                    }
+
+
+
+                }
 
             }).catch((err)=>{
             console.log(err);
