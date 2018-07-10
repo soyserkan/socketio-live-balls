067ecc2298c9604e3fc236255dbcdcef1e6344c3
soyserkan <serkansoy94@gmail.com>
2018-07-10 16:12:56 +0300

    spesifik ad

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,4 +1,4 @@
-app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=> {
+app.controller('indexController',['$scope','indexFactory','configFactory',($scope,indexFactory,configFactory)=> {
 
     $scope.messages=[];
     $scope.players={};
@@ -35,8 +35,9 @@ app.controller('indexController',['$scope','indexFactory',($scope,indexFactory)=
         };
         try {
 
-
-            const socket = await indexFactory.connectSocket('http://localhost:3000', connectionOptions);
+            const socketUrl=await configFactory.getConfig();
+            console.log();
+            const socket = await indexFactory.connectSocket(socketUrl.data.socketUrl, connectionOptions);
             socket.emit('newUser', {username});
 
             socket.on('initPlayers', (players) => {
